3e77b3576e10e8ab4683241515fdb608b459d13f
Balu_Mothukuri <balumotukuru@gmail.com>
2021-03-01 22:49:35 -0800

    feat : Add the logic to take input from the console.
    * Build a dictionary from the inputs taken from the console.

--- Finding_the_percentage.py
+++ Finding_the_percentage.py
@@ -1,10 +1,16 @@
 # Finding the percentage
 
+"""
+Find the average marks of a given student.
+"""
+
 from statistics import mean
 
 if __name__ == '__main__':
-    records = {
-        'alpha': [20, 30, 40],
-        'beta': [30, 50, 70]
-    }
-    print(mean(records.get('beta')))
+    records = {}
+    for i in range(int(input('Enter the number of students : '))):
+        name, score_str = input('Enter the student and scores : ').split(' ', 1)
+        scores = list(map(float, score_str.split()))
+        records[name] = scores
+    query_name = input('Enter the name of the student who average marks need to be calculated : ')
+    print("The average marks of the student {} is {:0.2f}".format(query_name, round(mean(records.get(query_name)),2)))
