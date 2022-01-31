0c8fe295bb93ed2501c2f185a90ee0ab2fcca436
Balu Mothukuri <balumotukuru@gmail.com>
2022-01-31 00:17:27 -0800

    Implemented the List comprehension method for swapping case
    "Corrected the global email address of the author"

--- SwapCase.py
+++ SwapCase.py
@@ -12,21 +12,37 @@ loop through chars in the input string.
 			and set it to result string.
 	else:
 		set it to the result string.
+
+call the swap_case() function (instead of swap_case_listcomp()) in main block to execute the list comprehension code.
+
 """
+
+
 def swap_case(word):
-	result = ""
-	for i in range(len(word)):
-		if word[i].isalpha():
-			if word[i].isupper():
-				result = result+word[i].lower()
-			elif word[i].islower():
-				result = result+word[i].upper()
-		else:
-			result = result+word[i]
-	return result
+    result = ""
+    for i in range(len(word)):
+        if word[i].isalpha():
+            if word[i].isupper():
+                result = result + word[i].lower()
+            elif word[i].islower():
+                result = result + word[i].upper()
+        else:
+            result = result + word[i]
+    return result
+
+
+"""
+Implement using list comprehension. 
+for each char in string, check if upper, then return lower else return upper.
+call the swap_case_listcomp function (instead of swap_case()) in main block to execute the list comprehension code.
+"""
+
+
+def swap_case_listcomp(word):
+    return "".join([char.lower() if char.isupper() else char.upper() for char in word])
 
 
 if __name__ == '__main__':
-	s = input()
-	result = swap_case(s)
-	print ("Swapped case result - " + result)
+    s = input('Enter the string to be swapped case : ')
+    result = swap_case_listcomp(s)
+    print ("Swapped case result - " + result)
